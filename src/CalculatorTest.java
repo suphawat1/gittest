6e8f6841dee6e4d6765e003ab25c52779d10e7d0
suphawat1 <suphawatcccc@gmail.com>
2026-07-07 02:25:26 +0700

    upload junit project

---
+++ src/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
